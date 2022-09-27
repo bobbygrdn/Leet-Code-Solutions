1b4e160cafe240fa717209477ac0328b03eadccf
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-27 08:23:11 -0700

    Time: 219 ms (12.16%), Space: 44.9 MB (39.47%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -4,13 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     
-    let map = {}
     for(let i = 0; i < nums.length; i++) {
-        if(map[nums[i]] === undefined) {
-            map[nums[i]] = 1;
-        } else {
-            nums.splice(i, 1)
-            i--;
+
+        if(nums[i] === nums[i+1]) {
+            nums.splice(i+1, 1)
+            i--
         }
     }
     return nums.length
